d284e872284bc84854e2a5ec6384697ab3705122
huanshenyi <txy1226052@gmail.com>
2019-08-04 15:59:05 +0900

    clientの新規ロジカル完成

--- app/api/v1/client.py
+++ app/api/v1/client.py
@@ -1,9 +1,46 @@
+from flask import request
+
 from app.libs.redprint import RedPrint
+from app.validators.forms import ClientForm, UserEmailForm
+from app.libs.enums import ClientTypeEnum
+
+from app.models.user import User
 
 api = RedPrint('client')
 
 
-@api.route('/register')
+@api.route('/register', methods=['POST'])
 def create_client():
+    data = request.json
+    form = ClientForm(data=data)
+    if form.validate():
+        promise = {
+          ClientTypeEnum.USER_EMAIL: __register_user_by_email,
+          ClientTypeEnum.USER_MINA: __register_user_by_mina
+        }
+        promise[form.type.data]()
+    return 'success'
+    # form json
+    # web  モバイル(webモバイル)
     # WTForms
+
+
+def __register_user_by_email():
+    """
+    emailの新規処理
+    :return:
+    """
+    form = UserEmailForm(data=request.json)
+    if form.validate():
+        User.register_by_email(form.nickname.data,
+                               form.account.data,
+                               form.secret.data)
+    pass
+
+
+def __register_user_by_mina():
+    """
+    wechatappの新規処理
+    :return:
+    """
     pass
--- app/config/secure.py
+++ app/config/secure.py
@@ -1,3 +1,7 @@
 """
 apiのデリカシーの内容保存用
 """
+
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:root@localhost/ginger'
+
+SECRET_KEY = r'\x88D\xf09\x91\x07\x98\x89\x87\x96\xa0A\xc68\xf9\xecJ:U\x17\xc5V\xbe\x8b\xef\xd7\xd8\xd3\xe6\x98*4'
